3b21f9f795ad601f039a38497a362f00ff5c4665
Sumit Sharma <sumit.sharma@clustervision.com>
2023-08-22 11:15:45 +0200

    TRIX-168 Switch Application Clean and Lint

--- switch/app.py
+++ switch/app.py
@@ -164,5 +164,5 @@ def clone(record=None):
 
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', port= 7059, debug= True)
-    # app.run()
+    # app.run(host= '0.0.0.0', port= 7059, debug= True)
+    app.run()
