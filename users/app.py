78a06b24048fad6984a6cd23987128f2b9f2080d
Diego Sonaglia <diego.sonaglia@clustervision.com>
2023-08-28 12:08:06 +0200

    basic user app template working

--- users/app.py
+++ users/app.py
@@ -13,79 +13,16 @@ __maintainer__  = 'Diego Sonaglia'
 __email__       = 'diego.sonaglia@clustervision.com'
 __status__      = 'Development'
 
-
-import datetime
-from flask import Flask, render_template, redirect, request
-from sensu_requests import SensuRequestHandler
-from config import sensu_settings
-from utils import prettify_relative_time
-
+from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/')
-handler = SensuRequestHandler(sensu_settings=sensu_settings)
-
 
 @app.route("/")
 def index():
     """
     This is main route of application, it will serve index page of the application.
     """
-    return render_template('index.html', sensu_settings=sensu_settings)
-
-
-@app.route("/checks")
-def checks():
-    """
-    This API will get all the checks.
-    """
-    items = handler.get_checks()
-    current_time = datetime.datetime.utcnow()
-    return render_template('checks_table.html', items=items, time=current_time)
-
-
-@app.route("/events")
-def events():
-    """
-    This API will get all the events.
-    """
-    items = handler.get_events()
-    current_time = datetime.datetime.utcnow()
-    return render_template('events_table.html', items=items, time=current_time)
-
-
-@app.route("/silenced")
-def silenced():
-    """
-    This API will get all the silenced.
-    """
-    items = handler.get_silenced()
-    current_time = datetime.datetime.utcnow()
-    return render_template('silenced_table.html', items=items, time=current_time)
-
-
-@app.route("/dashboard")
-def dashboard():
-    """
-    This API will show the dashboard.
-    - Kind of a hacky workaround to get the dashboard url
-    """
-    if sensu_settings.ood_use_tls:
-        schema = 'https'
-    else:
-        schema = 'http'
-    return redirect(f"{schema}://{request.host}")
-
-
-@app.template_filter('prettify_ts')
-def prettify_ts(timestamp=None):
-    """
-    This API will get the prettify relative time.
-    """
-    target_date = datetime.datetime.fromtimestamp(timestamp)
-    current_date = datetime.datetime.now()
-    delta = (current_date - target_date).seconds
-    return prettify_relative_time(delta)
-
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
