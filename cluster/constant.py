ec50a3c721836b5e00218e357e7268778780df2f
Sumit Sharma <sumit.sharma@clustervision.com>
2023-10-02 11:02:03 +0200

    NOJIRA :: Change the INI file location

--- cluster/constant.py
+++ cluster/constant.py
@@ -13,7 +13,7 @@ __email__       = "sumit.sharma@clustervision.com"
 __status__      = "Development"
 
 INI_FILE = '/trinity/local/ondemand/3.0/config/luna.ini'
-TOKEN_FILE = '/tmp/token.txt'
+TOKEN_FILE = '/trinity/local/ondemand/3.0/config/token.txt'
 LOG_DIR = '/var/log/luna'
 LOG_FILE = '/var/log/luna/luna2-web.log'
 
--- service/constant.py
+++ service/constant.py
@@ -13,5 +13,5 @@ __email__       = "sumit.sharma@clustervision.com"
 __status__      = "Development"
 
 INI_FILE = '/trinity/local/ondemand/3.0/config/luna.ini'
-TOKEN_FILE = '/tmp/token.txt'
+TOKEN_FILE = '/trinity/local/ondemand/3.0/config/token.txt'
 LOG_FILE = '/var/log/luna/luna2-web.log'
