11734552e4116d006d2fbeb3bf0e9f7d6e6aa5c4
Sumit Sharma <sumit.sharma@clustervision.com>
2024-09-27 17:51:02 +0200

    TRIX-510 :: Errors fixed, Manage Rack frame is pending

--- rack/app.py
+++ rack/app.py
@@ -380,5 +380,5 @@ def license_info():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=7059, debug=True)
-    # app.run()
+    # app.run(host='0.0.0.0', port=7059, debug=True)
+    app.run()
