5570f06a6bd75929d0633190ab5cc0168ea68601
Sumit Sharma <sumit.sharma@clustervision.com>
2025-03-03 16:27:21 +0100

    TRIX-780 :: Service application added app state & error handling 404 [Tested Manually] [Ready to Merge]
    KEEP MY BRANCH

--- service/constant.py
+++ service/constant.py
@@ -41,3 +41,4 @@ else:
 INI_FILE = '/trinity/local/ondemand/3.0/config/luna.ini'
 LICENSE = '/trinity/local/ondemand/3.0/LICENSE.txt'
 LOG_FILE = '/var/log/luna/luna2-web.log'
+APP_STATE = True  # False for Development, True for Production
