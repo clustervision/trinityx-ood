862f3765e94088c376aedd9e2b21fd173c75dfc5
yixin <yixin.zhang@clustervision.com>
2026-04-30 13:39:06 +0200

    TRIX-1743 :: change route path

--- group/app.py
+++ group/app.py
@@ -111,7 +111,7 @@ def home():
     )
 
 
-@app.route('/', methods=['GET'])
+@app.route('/groups', methods=['GET'])
 def api_groups():
     """
     JSON list of all groups for the Vue frontend table.
