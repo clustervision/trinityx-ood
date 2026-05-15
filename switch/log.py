0919ef6d4b689c6f1c091d971ded0873399f137d
Sumit Sharma <sumit.sharma@clustervision.com>
2026-05-15 12:13:01 +0200

    TRIX-838 :: Switch Backend is Ready
    KEEP MY BRANCH

--- switch/log.py
+++ switch/log.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # This code is part of the TrinityX software suite
-# Copyright (C) 2023  ClusterVision Solutions b.v.
+# Copyright (C) 2026  ClusterVision Solutions b.v.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,13 +24,13 @@ Method get_logger will provide a logging object, which is helpful to write the l
 Logger Object have basic methods: debug, error, info, critical and warnings.
 
 """
-__author__      = 'Sumit Sharma'
-__copyright__   = 'Copyright 2022, Luna2 Project[OOD]'
-__license__     = 'GPL'
-__version__     = '2.0'
-__maintainer__  = 'Sumit Sharma'
-__email__       = 'sumit.sharma@clustervision.com'
-__status__      = 'Development'
+__author__      = "Sumit Sharma"
+__copyright__   = "Copyright 2026, Luna2 Project [OOD]"
+__license__     = "GPL"
+__version__     = "3.0"
+__maintainer__  = "Sumit Sharma"
+__email__       = "sumit.sharma@clustervision.com"
+__status__      = "Production"
 
 import sys
 import logging
@@ -41,33 +41,33 @@ class Log:
     """
     This Log Class is responsible to start the Logger depend on the Level.
     """
-    __logger = None
+    __logger = logging.getLogger()
 
 
     @classmethod
-    def init_log(cls, log_level=None):
+    def init_log(cls, log_level: str):
         """
         Input - log_level
         Process - Validate the Log Level, Set it to INFO if not correct.
         Output - Logger Object.
         """
         levels = {'NOTSET': 0, 'DEBUG': 10, 'INFO': 20, 'WARNING': 30, 'ERROR': 40, 'CRITICAL': 50}
-        log_level = levels[log_level.upper()]
+        level = levels[log_level.upper()]
         thread_level = '[%(levelname)s]:[%(asctime)s]:[%(threadName)s]:'
         message = '[%(filename)s:%(funcName)s@%(lineno)d] - %(message)s'
         log_format = f'{thread_level}{message}'
         try:
-            logging.basicConfig(filename=LOG_FILE, format=log_format, filemode='a', level=log_level)
+            logging.basicConfig(filename=LOG_FILE, format=log_format, filemode='a', level=level)
             cls.__logger = logging.getLogger('luna2-cli')
-            cls.__logger.setLevel(log_level)
-            if log_level == 10:
+            cls.__logger.setLevel(level)
+            if level == 10:
                 formatter = logging.Formatter(log_format)
                 console = logging.StreamHandler(sys.stdout)
-                console.setLevel(log_level)
+                console.setLevel(level)
                 console.setFormatter(formatter)
                 cls.__logger.addHandler(console)
             levels = {0:'NOTSET', 10: 'DEBUG', 20: 'INFO', 30: 'WARNING', 40:'ERROR', 50:'CRITICAL'}
-            cls.__logger.info(f'####### Luna Logging Level IsSet To [{levels[log_level]}] ########')
+            cls.__logger.info("####### Alertx Logging Level IsSet To [%s] ########", levels[level])
             return cls.__logger
         except PermissionError:
             sys.stderr.write('ERROR :: Run this tool as a super user.\n')
@@ -82,17 +82,6 @@ class Log:
         """
         return cls.__logger
 
-    @classmethod
-    def set_logger(cls, log_level=None):
-        """
-        Input - None
-        Process - Update the existing Log Level
-        Output - Logger Object.
-        """
-        levels = {'NOTSET': 0, 'DEBUG': 10, 'INFO': 20, 'WARNING': 30, 'ERROR': 40, 'CRITICAL': 50}
-        log_level = levels[log_level.upper()]
-        cls.__logger.setLevel(log_level)
-        return cls.__logger
 
     @classmethod
     def check_loglevel(cls):
