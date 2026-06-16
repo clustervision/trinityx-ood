e4d6f8ee082797b53ba23767246f00bbc7815911
Yixin Zhang <yixin.zhang@clustervision.com>
2026-06-16 16:48:42 +0200

    TRIX-1879 :: Apply feedback fixes for alertx, dns, node, osimage and rack modules.

--- osimage/app.py
+++ osimage/app.py
@@ -204,6 +204,16 @@ def api_v1_check_status(status, request_id):
     return Rest.forward_daemon_response(resp)
 
 
+@app.route('/license', methods=['GET'])
+def license_info():
+    """Human-readable license page (footer link opens this in a new tab)."""
+    response = 'LICENSE Information is not available at this moment.'
+    if os.path.isfile(LICENSE) and os.access(LICENSE, os.R_OK):
+        with open(LICENSE, 'r', encoding="utf-8") as file_data:
+            response = '<br />'.join(file_data.readlines())
+    return response
+
+
 @app.route('/api/v1/license', methods=['GET'])
 def api_v1_license():
     file_check = os.path.isfile(LICENSE)
